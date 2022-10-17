da6ec556a764b7107045ab185caff48b82ffbc90
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2022-10-17 12:59:28 +0100

    Update css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const details = document.querySelectorAll('details');
-const summary = document.querySelectorAll('summary');
+// const details = document.querySelectorAll('details');
+const summary = document.querySelectorAll('.summary');
 
-summary.addEventListener('click', function () {
+summary.addEventListener('click', summaryToggle)
+
+const summaryToggle = function () {
     summary.classList.toggle('active')
-})
+}
